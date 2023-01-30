6794ba51355c7b95c7812a36bfc0945e7d6ef995
枫霜月雨 <13611667101@163.com>
2023-01-30 14:26:34 +0800

    Revert "Update start-CTFd.sh"
    This reverts commit c600f86a48aa59aaaf6ca49ad3ceec944321d2e4.

--- start-CTFd.sh
+++ start-CTFd.sh
@@ -2,8 +2,7 @@
 #CTFd
 #git clone https://ghproxy.com/https://github.com/liuxin2020/CTFd
 
-git clone https://ghproxy.com/https://github.com/liuxin2020/ctfd-plugin-choice CTFd/plugins/ctfd-plugin-choice --depth=1
-git clone https://ghproxy.com/https://github.com/liuxin2020/ctfd-plugin-multichoice CTFd/plugins/ctfd-plugin-multichoice --depth=1
-git clone https://ghproxy.com/https://github.com/frankli0324/ctfd-whale CTFd/plugins/ctfd-whale --depth=1
+git clone https://ghproxy.com/https://github.com/liuxin2020/ctfd-plugin-choice CTFd/plugins/ctfd-plugin-choice
+git clone https://ghproxy.com/https://github.com/liuxin2020/ctfd-plugin-multichoice CTFd/plugins/ctfd-plugin-multichoice
+git clone https://ghproxy.com/https://github.com/frankli0324/ctfd-whale CTFd/plugins/ctfd-whale
 
-chmod +x docker-entrypoint.sh
